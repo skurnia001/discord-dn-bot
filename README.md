609bf761c5144606e2235ad65b4154aa14f196d4
skurnia001 <steven.kurnia23@gmail.com>
2020-11-07 07:47:23 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Dragon Nest Dragon Jade Query Discord Bot
+
+[Dragon Nest](https://en.wikipedia.org/wiki/Dragon_Nest)
+
+Dragon Jade is a special item in Dragon Nest which is useful for increasing damage of a character. 
+Each class in Dragon Nest has their own skills which is upgraded by dragon jade. 
+There are also multiple variants of dragon jade.
+
+This version of bot is using skill naming for SEA server, other server might differ in skill naming, 
+so the bot might not work.
+
+As per this recurring joke in Computer Science.
+![Solving 1 minute routine task by making script that takes 20 minutes to develop](https://imgs.xkcd.com/comics/the_general_problem.png)
---
+++ bot.py
@@ -0,0 +1,34 @@
+# bot.py
+import os
+import string
+import csv
+from dotenv import load_dotenv
+from discord.ext import commands
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+bot = commands.Bot(command_prefix='/')
+
+dic = dict()
+with open('skill.csv', 'r', newline='', encoding='utf-8-sig') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        dic[row[0]] = {"class": row[1], "jade": row[2]}
+
+
+@bot.command(name='jsq', aliases=['jadeskillquery', ], help='Finds a class whose skill match the skill name given '
+                                                            'if they have that skill as the skill of some dragon jade.')
+async def jsq(ctx, *args):
+    skill = ' '.join(args)
+    skill = string.capwords(skill)
+    if skill in dic:
+        response = "This is {}'s skill for {}".format(dic[skill]['class'], dic[skill]['jade'])
+    else:
+        response = "No jade found using this skill :("
+
+    await ctx.send(response)
+
+
+bot.run(TOKEN)
