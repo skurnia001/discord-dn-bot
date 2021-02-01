b80d0e77e24908fbec6fe17a57f2d6bb3d4751e2
Steven Kurnia <skurnia001@e.ntu.edu.sg>
2021-02-02 00:22:48 +0800

    Fix issue #1 #2 #4

--- bot.py
+++ bot.py
@@ -14,14 +14,14 @@ dic = dict()
 with open('skill.csv', 'r', newline='', encoding='utf-8-sig') as f:
     reader = csv.reader(f)
     for row in reader:
-        dic[row[0]] = {"class": row[1], "jade": row[2]}
+        dic[row[0].lower()] = {"class": row[1], "jade": row[2]}
 
 
 @bot.command(name='jsq', aliases=['jadeskillquery', ], help='Finds a class whose skill match the skill name given '
                                                             'if they have that skill as the skill of some dragon jade.')
 async def jsq(ctx, *args):
     skill = ' '.join(args)
-    skill = string.capwords(skill)
+    skill = skill.lower()
     if skill in dic:
         response = "This is {}'s skill for {}".format(dic[skill]['class'], dic[skill]['jade'])
     else:
