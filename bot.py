55f5baf571a85ea0731b314dd66b98a9378b8887
Hans Tananda <hanstananda@gmail.com>
2023-01-29 22:21:18 +0700

    added config file to disable services, fix db warning message, logging fix and refactor (#32)

--- bot.py
+++ bot.py
@@ -1,7 +1,10 @@
+#!/usr/bin/env python
 # bot.py
 import asyncio
 import logging
 import logging.handlers
+import sys
+import yaml
 
 import discord
 import os
@@ -25,12 +28,27 @@ intents.message_content = True
 master_bot = commands.Bot(command_prefix=('/',), intents=intents)
 
 
-async def setup(bot: commands.Bot) -> None:
-    await bot.add_cog(DNSkillQuery(bot))
-    await bot.add_cog(Venti(bot))
-    await bot.add_cog(DNReminderEventBot(bot, CHANNEL_ID))
-    await bot.add_cog(DNSTGReq(bot))
-    await bot.add_cog(DNDungeonKilos(bot))
+async def setup(bot: commands.Bot, config: dict) -> None:
+    logger = setup_logger('bot_setup')
+    if not 'DNSkillQuery' in config['DisabledServices']:
+        await bot.add_cog(DNSkillQuery(bot))
+        logger.info("DNSkillQuery module added successfully")
+    
+    if not 'Venti' in config['DisabledServices']:
+        await bot.add_cog(Venti(bot))
+        logger.info("Venti module added successfully")
+    
+    if not 'DNReminderEventBot' in config['DisabledServices']:
+        await bot.add_cog(DNReminderEventBot(bot, CHANNEL_ID))
+        logger.info("DNReminderEventBot module added successfully")    
+
+    if not 'DNSTGReq' in config['DisabledServices']:
+        await bot.add_cog(DNSTGReq(bot))
+        logger.info("DNSTGReq module added successfully")    
+
+    if not 'DNDungeonKilos' in config['DisabledServices']:
+        await bot.add_cog(DNDungeonKilos(bot))
+        logger.info("DNDungeonKilos module added successfully")    
 
 
 @master_bot.command()
@@ -44,25 +62,45 @@ async def sync(ctx: Context) -> None:
     )
 
 
-async def main() -> None:
-    await setup(master_bot)
-
-    logger = logging.getLogger('discord')
+# Logger setup 
+def setup_logger(name='discord') -> logging.Logger:
+    logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
+    dt_fmt = '%Y-%m-%d %H:%M:%S'
+    formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')
 
-    handler = logging.handlers.RotatingFileHandler(
+    file_handler = logging.handlers.RotatingFileHandler(
         filename='log/discord.log',
         encoding='utf-8',
         maxBytes=4 * 1024 * 1024,  # 1 MiB
         backupCount=5,  # Rotate through 5 files
     )
-    dt_fmt = '%Y-%m-%d %H:%M:%S'
-    formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
+    file_handler.setFormatter(formatter)
+
+    console_handler = (logging.StreamHandler(sys.stdout)) # Mirror logs to stdout for journalctl to catch and for debugging
+    console_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+    return logger
+
+async def main() -> None:
+    logger = setup_logger()
+
+    # Load Config 
+    config=dict()
+    try:
+        with open("config.yaml", "r") as stream:
+            config= yaml.safe_load(stream)
+    except FileNotFoundError or yaml.YAMLError as exc:
+        logging.warning(f"Unable to load config file. Reverting to default... Error is {exc}")
+    if 'DisabledServices' not in config:
+        config['DisabledServices']=list()
+
+    await setup(master_bot, config)
 
     async with master_bot:
-        print("Bot started...")
+        logger.info("Bot started...")
         await master_bot.start(TOKEN)
 
 
